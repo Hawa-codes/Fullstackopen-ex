5041e5b7e97ccfa4b60dc027b3c10a67ac49ef75
Hawa-codes <adediranhawa@gmail.com>
2026-06-27 21:20:43 +0100

    anaedote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,17 +2,17 @@ import { useState } from "react";
 import Anecdote from "./Anecdote";
 
 function App() {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later.',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place...',
-  ];
+  // const anecdotes = [
+  //   'If it hurts, do it more often.',
+  //   'Adding manpower to a late software project makes it later.',
+  //   'The first 90 percent of the code accounts for the first 90 percent of the development time...',
+  //   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  //   'Premature optimization is the root of all evil.',
+  //   'Debugging is twice as hard as writing the code in the first place...',
+  // ];
 
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+  // const [selected, setSelected] = useState(0);
+  // const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const handleNext = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
