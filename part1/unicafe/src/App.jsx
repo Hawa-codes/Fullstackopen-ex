49665ee2d747afc9fcd3b09bb88a3e4f0be727f8
Hawa-codes <adediranhawa@gmail.com>
2026-07-18 23:55:30 +0100

    part 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,16 +16,23 @@ const Statistics = ({good, neutral, bad, all, average, positive}) => {
         <StatisticLine text="good" value={good} />
         <StatisticLine text="neutral" value={neutral} />
         <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="bad" value={all} />
-        <StatisticLine text="bad" value={average} />
-        <StatisticLine text="bad" value={positive} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={`${positive}%`} />
       </tbody>
     </table>
   )
 }
 
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -37,9 +44,9 @@ const App = () => {
   return (
     <div>
       <h2>give Feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h4>statistics</h4>
       {all === 0 ? (
         <p>No feedback given yet</p>
