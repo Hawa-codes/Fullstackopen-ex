0cf9dfc03d6acd34aca0e9a149d5119de1c8e883
Hawa-codes <adediranhawa@gmail.com>
2026-07-20 18:37:57 +0100

    Complete Course Information

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+
+  return (
+    <p>
+      <strong>Total of {total} exercises</strong>
+    </p>
+  );
+};
 
 const Course = ({ course }) => {
   return (
@@ -11,6 +41,6 @@ const Course = ({ course }) => {
       <Total parts={course.parts} />
     </div>
   );
-}
+};
 
 export default Course;
