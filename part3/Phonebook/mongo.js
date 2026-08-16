90cb1752982c7bb561f3c125a7ae211255102e3c
Hawa-codes <adediranhawa@gmail.com>
2026-08-16 20:56:25 +0100

    Full stack open

--- part3/Phonebook/mongo.js
+++ part3/Phonebook/mongo.js
@@ -17,7 +17,7 @@ const personSchema = new mongoose.Schema({
         required: true
     },
     number: {
-        type: Number,
+        type: String,
         required: true
     }
 });
@@ -27,12 +27,24 @@ const Person = mongoose.model("Person", personSchema);
 const name = process.argv[2];
 const number = process.argv[3];
 
-const person = new Person({
-  name,
-  number,
-});
+if (name && number) {
+  const person = new Person({
+    name,
+    number,
+  });
 
-person.save().then(() => {
-  console.log(`added ${name} number ${number} to phonebook`);
-  mongoose.connection.close();
-});
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((persons) => {
+    console.log("phonebook:");
+
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+
+    mongoose.connection.close();
+  });
+}
